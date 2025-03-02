39f9de2113159717669a0377adfb75aaea93477a
Aman Mishra <amanmishra.5272@gmail.com>
2025-03-02 21:23:29 +0530

    Add CallToAction component to Home page for user engagement

---
+++ client/src/components/students/CallToAction.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { assets } from "../../assets/assets";
+
+const CallToAction = () => {
+  return (
+    <div className="flex flex-col items-center gap-4 pt-10 pb-24 px-8 md:px-0">
+      <h1 className="text-xl md:text-4xl text-gray-800 font-semibold">
+        Ready to start your journey to success?
+      </h1>
+      <p className="text-gray-500 sm:text-sm">
+        Join our community of learners and take your skills to the next level.
+        <br />
+        Start your journey to success today.
+      </p>
+      <div className="flex items-center gap-6 font-medium mt-4 justify-center">
+        <button className="bg-blue-600 text-white px-10 py-3 rounded-md">
+          Get Started
+        </button>
+        <button className="px-10 py-3 rounded-md flex items-center gap-2 bg-white text-black border border-gray-300">
+          Learn More <img src={assets.arrow_icon} alt="arrow_icon" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CallToAction;
--- client/src/pages/students/Home.jsx
+++ client/src/pages/students/Home.jsx
@@ -3,7 +3,7 @@ import Hero from '../../components/students/Hero'
 import Companies from '../../components/students/Companies'
 import CourseSection from '../../components/students/CourseSection'
 import TestinomialsSection from '../../components/students/TestinomialsSection'
-
+import CallToAction from '../../components/students/CallToAction'
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
@@ -11,6 +11,7 @@ const Home = () => {
       <Companies/>
       <CourseSection/>
       <TestinomialsSection/>
+      <CallToAction/>
     </div>
   )
 }
