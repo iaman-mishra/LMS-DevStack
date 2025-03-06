502795e71ba173f8959eead6621d094042f0edcd
Aman Mishra <amanmishra.5272@gmail.com>
2025-03-06 14:19:13 +0530

    Refactor Navbar component for improved navigation and user interaction

--- client/src/components/students/Navbar.jsx
+++ client/src/components/students/Navbar.jsx
@@ -1,53 +1,80 @@
 import React, { useContext } from "react";
 import { assets } from "../../assets/assets.js";
 import { Link, useLocation } from "react-router-dom";
-import { useClerk, useUser, SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-import logo from '../../assets/logo.png'
+import {
+  useClerk,
+  useUser,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import logo from "../../assets/logo.png";
 import { AppContext } from "../../context/AppContext.jsx";
 
 const Navbar = () => {
-
   const location = useLocation();
-  const isCouseListPage = location.pathname.includes('/course-list')
+  const isCouseListPage = location.pathname.includes("/course-list");
   const { openSignIn } = useClerk();
   const { user } = useUser();
-  const {navigate, isEducator, SetIsEducator} = useContext(AppContext);
+  const { navigate, isEducator, SetIsEducator } = useContext(AppContext);
 
   return (
-    <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCouseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
-      <div className="flex items-center gap-1" >
+    <div
+      className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${
+        isCouseListPage ? "bg-white" : "bg-cyan-100/70"
+      }`}
+    >
+      <div className="flex items-center gap-1 " onClick={() => navigate("/")}>
         <img
           src={logo}
           alt="logo"
           className=" h-[30px] sm:h-[35px] cursor-pointer"
-          onClick={()=>navigate('/')}
         />
-        <h1 className="font-bold text-gray-700 text-lg sm:text-2xl ">DevStack-LMS</h1>
+        <h1 className="font-bold text-gray-700 text-lg sm:text-2xl ">
+          DevStack-LMS
+        </h1>
       </div>
       <div className="hidden md:flex items-center gap-5 text-gray-500">
         <div className="flex items-center gap-5">
-          {user &&
+          {user && (
             <>
-              <button onClick={()=>navigate('/educator')}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
+              <button onClick={() => navigate("/educator")}>
+                {isEducator ? "Educator Dashboard" : "Become Educator"}
+              </button>
               | <Link to="/my-enrollments">My Enrollments</Link>
             </>
-          }
+          )}
         </div>
-        {user ? <UserButton /> : <button onClick={() => openSignIn()} className="bg-blue-600 text-white px-5 py-2 rounded-full">
-          Create Account
-        </button>}
+        {user ? (
+          <UserButton />
+        ) : (
+          <button
+            onClick={() => openSignIn()}
+            className="bg-blue-600 text-white px-5 py-2 rounded-full"
+          >
+            Create Account
+          </button>
+        )}
       </div>
       <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
         <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
-          {user &&
+          {user && (
             <>
-              <button onClick={()=>navigate('/educator')}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
+              <button onClick={() => navigate("/educator")}>
+                {isEducator ? "Educator Dashboard" : "Become Educator"}
+              </button>
               | <Link to="/my-enrollments">My Enrollments</Link>
             </>
-          }
+          )}
         </div>
-        {user ? <UserButton/> : <button onClick={()=>openSignIn()}><img src={assets.user_icon} alt="" /></button>}
-        
+        {user ? (
+          <UserButton />
+        ) : (
+          <button onClick={() => openSignIn()}>
+            <img src={assets.user_icon} alt="" />
+          </button>
+        )}
       </div>
     </div>
   );
