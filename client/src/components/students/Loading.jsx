9451260ea21ad7dee6d5b014cc0a6d23b6acded7
Aman Mishra <amanmishra.5272@gmail.com>
2025-03-06 14:16:01 +0530

    Add Vercel routing configuration and enhance Loading component with spinner

--- client/src/components/students/Loading.jsx
+++ client/src/components/students/Loading.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div>Loading</div>
+    <div>
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-10 h-10 border-t-transparent border-solid rounded-full animate-spin border-blue-500 border-8"></div>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/students/Home.jsx
+++ client/src/pages/students/Home.jsx
@@ -4,6 +4,7 @@ import Companies from '../../components/students/Companies'
 import CourseSection from '../../components/students/CourseSection'
 import TestinomialsSection from '../../components/students/TestinomialsSection'
 import CallToAction from '../../components/students/CallToAction'
+
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
