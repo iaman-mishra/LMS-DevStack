6b0084a69d71a0c043b894be779f7898f287d62d
Aman Mishra <amanmishra.5272@gmail.com>
2025-03-31 02:07:12 +0530

    Add functionality to retrieve course details by ID with educator information

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -10,3 +10,22 @@ export const getAllCourses = async (req, res)=>{
         res.json({sucess:false, message:error.message});
     }
 }
+
+export const getCourseId = async (req,res)=>{
+    const {id} = req.params
+    try {
+        const courseData = await Course.findById(id).populate({path:'educator'})
+        // remove url if preview fasle
+        courseData.courseContent.forEach(chapter => {
+            chapter.chapterContent.forEach(lecture => {
+                if (!lecture.isPreviewFree) {
+                    lecture.lectureUrl = '',
+
+                }
+            })
+        })
+        res.json({sucess:true, courseData});
+    } catch (error) {
+        res.json({sucess:false, message:error.message});
+    }
+}
